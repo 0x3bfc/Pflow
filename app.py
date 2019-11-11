85a3cf88a8ffeccf1695b23ffaf5ad9d590e0715
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2019-11-11 16:42:03 +0100

    documentation

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ from flask import render_template
 from network.simple_network import SimpleNetwork
 
 app = Flask(__name__)
+simple_net = SimpleNetwork()
 
 
 @app.route('/<string:element>/', methods=['GET'])
-def get_load_results(element):
+def get_results(element):
+    """
+    get the cached results of a component. It will return 404 page
+    if the component doesn't exist
+    :param element: topology component (load, generator, line, bus, etc)
+    :return: html response that has the component results
+    """
     if element == 'generator':
         element = 'gen'
     try:
@@ -17,15 +24,19 @@ def get_load_results(element):
 
 @app.route('/', methods=['GET'])
 def index():
+    """
+    loads the index.html
+    :return:  index.html
+    """
     return render_template('index.html')
 
 
 if __name__ == '__main__':
     # setup network
-    simple_net = SimpleNetwork()
     simple_net.setup()
     # calc power flow
-    simple_net.calc()
-    simple_net.render_to_html()
+    simple_net.run()
+    # generate index.html
+    simple_net.render()
     # start server
     app.run(debug=True, host='0.0.0.0', port=8000)
--- network/simple_network.py
+++ network/simple_network.py
@@ -3,10 +3,26 @@ from pandapower.plotting import to_html
 
 
 class SimpleNetwork:
-    # create empty net
-    net = pp.create_empty_network()
+    """SimpleNetwork class is the entry point into simple network topology"""
+
+    def __init__(self):
+        """
+        Initialize SimpleNetwork Class by creating new empty network
+        This class has a hard-coded topology parameters.
+        This topology has 3 buses, one generator 0.2MWs, a load, an
+        external grid, and a transformer and wired as follows:
+            * The external grid is connected to bus1
+            * The load and the generator is connected to bus3
+            * A transformer is connected to bus2 and bus1
+            * Finally, A 100 meters line is connected to bu2, bus3
+        """
+        self.net = pp.create_empty_network()
 
     def setup(self):
+        """
+        Setup the topology, parameters are hardcoded.
+        :return: network object that holds the topology
+        """
         # create buses
         bus1 = pp.create_bus(self.net, vn_kv=20., name="Bus 1")
         bus2 = pp.create_bus(self.net, vn_kv=0.4, name="Bus 2")
@@ -22,12 +38,24 @@ class SimpleNetwork:
 
         return self.net
 
-    def calc(self):
+    def run(self):
+        """
+        calculate the power flow results. this results will cached
+        and served through APIs.
+        :return: power flow results
+        """
         return pp.runpp(self.net)
 
     def get(self, element):
+        """
+        retrieves the cached results for each component in the topology
+        :return: results for specific component
+        """
         return eval('self.net.res_' + element + '.to_html()')
 
-    def render_to_html(self):
+    def render(self):
+        """
+        Generate an index.html file which contains the whole topology results
+        """
         to_html(self.net, './templates/index.html')
 
